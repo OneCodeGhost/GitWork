3bcffadc44ca8c68d703587f6be4e7d6bb31308b
CodeGhost <458661505@qq.com>
2016-10-15 09:38:58 +0800

    抽奖程序

--- 随机抽取/cjjs.js
+++ 随机抽取/cjjs.js
@@ -1,5 +1,6 @@
 var data=["A","B","C","D","E","F","G"],
-	timer=null;
+	timer=null,
+	flag=0;
 
 window.onload=function(){
 	var play=document.getElementById("play"),
@@ -7,16 +8,31 @@ window.onload=function(){
 	//鼠标事件
 	play.onclick=playfun;
 	end.onclick=endfun;
+
+	//键盘事件
+	document.onkeyup=function(event){
+		event=event||window.event;
+		if(event.keyCode==32){
+			if(flag==0){
+				playfun();
+				flag=1;
+			}else{
+				endfun();
+				flag=0;
+			}
+		}
+	}
 }
 
 function playfun(){
 	var title=document.getElementById("title");
+	var play=document.getElementById("play");
 	clearInterval(timer);
 	timer=setInterval(function(){
 		var random=Math.floor(Math.random()*data.length);
 		title.innerHTML=data[random];
 	},50)
-	this.style.background="#999";
+	play.style.background="#999";
 }
 
 function endfun(){
